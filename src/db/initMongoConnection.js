1ae2103f94d3801b0cc3f2bb5824fb494d65b7dc
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-03-06 07:48:19 +0200

    fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
-const URL_DB = process.env.URL_DB;
-
-if (!URL_DB) {
-  throw new Error('❌ MONGO_URI is not defined in .env file!');
-}
 
 export async function initMongoConnection() {
   try {
+    const URL_DB = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+    if (!URL_DB) {
+      throw new Error('❌ MONGO_URI is not defined in .env file!');
+    }
     await mongoose.connect(URL_DB);
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
