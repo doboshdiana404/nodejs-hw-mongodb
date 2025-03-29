9d288f28daf424cc920a0f1356c9db72b7eebfc4
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-03-29 15:02:02 +0200

    remove Role and fix findId

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,8 +5,3 @@ export const SORT_ORDER = {
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAY = 30 * 24 * 60 * 60 * 1000;
-
-export const ROLES = {
-  TEACHER: 'teacher',
-  PARENT: 'parent',
-};
